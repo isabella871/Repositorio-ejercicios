69dbde2fed626b45c6a7eb1db87db55c71b82e74
Isabella Bernal C <bellabernalcarvajal871@gmail.com>
2025-02-23 19:18:09 -0500

    nose que paso

--- BlackJack&Semaforo/semaforo.js
+++
@@ -1,8 +0,0 @@
-let colorSemaforo= "luces intermitentes"
-
-let mensaje = colorSemaforo == "rojo" ? "parar" :
-colorSemaforo == "verde" ? "siga" :colorSemaforo == "amarillo" ? "preparar" : 
-colorSemaforo == "luces intermitentes" ? "apagado" :
-colorSemaforo == "cambio luces" ? "encendido" : "estado desconocido"
-
-console.log(mensaje)
