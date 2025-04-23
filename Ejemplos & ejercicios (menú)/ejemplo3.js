8778c71eb98d0b28aa82d1c294add1249c8aa50e
Isabella Bernal C <bellabernalcarvajal871@gmail.com>
2025-04-22 20:03:29 -0500

    .

--- Ejemplos & ejercicios (menú)/ejemplo3.js
+++
@@ -1,6 +0,0 @@
-let limitePersonas = false;
-let nombreSalon = "d";
-
-let mensaje = limitePersonas ? 'El salon llamado ' +nombreSalon +'  esta ocupado"' :
-    'el salon llamado ' + nombreSalon + ' esta disponible"';
-console. log(mensaje);
--- Ejemplos & ejercicios (menú)/ejercicio6.js
+++
@@ -1,9 +0,0 @@
-alert("Bienvenido, porfavor ingrese tres números")
-let num1=prompt("Ingrese un número:");
-let num2=prompt("Ingrese un número:");
-let num3=prompt("Ingrese un número:");
-
-let mayor= Math.max(num1, num2, num3);
-alert("El número mayor es: " + mayor);
-
-
